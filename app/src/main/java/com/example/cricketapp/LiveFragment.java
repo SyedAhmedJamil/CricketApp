3e63c675bc9dd4464a39026c1c5acfe98e2a1045
Syed Ahme Jamil 2 <ahmed.jamil112233@gmail.com>
2020-09-16 19:38:08 +0500

    new borders

--- app/src/main/java/com/example/cricketapp/LiveFragment.java
+++ app/src/main/java/com/example/cricketapp/LiveFragment.java
@@ -2,13 +2,20 @@ package com.example.cricketapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
@@ -20,6 +27,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class LiveFragment extends Fragment {
 
     MatchDetailsListAdapter adapter;
+    ViewPager2 viewPager2;
+    ViewPager2Adapter viewPager2Adapter;
+    TabLayout tabLayout;
 
     public LiveFragment() {
         // Required empty public constructor
@@ -27,40 +37,67 @@ public class LiveFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recents, container, false);
-
-        RecyclerView recyclerView = view.findViewById(R.id.match_details_recyclerview);
-        adapter = new MatchDetailsListAdapter();
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        View view = inflater.inflate(R.layout.fragment_live, container, false);
         return view;
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        viewPager2 = view.findViewById(R.id.pager);
+        viewPager2Adapter = new ViewPager2Adapter(this);
+        viewPager2.setAdapter(viewPager2Adapter);
+        tabLayout = view.findViewById(R.id.live_tabs);
+        new TabLayoutMediator(tabLayout,viewPager2,false,true,(tab, position) -> {
+            switch (position)
+            {
+                case 0:
+                    tab.setText("LIVE");
+                    break;
+                case 1:
+                    tab.setText("SCOREBOARD");
+                    break;
+                case 2:
+                    tab.setText("INFO");
+                    break;
 
-        OkHttpClient unsafeOkHttpClient = UnsafeOkHttpClient.getUnsafeOkHttpClient();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.crickssix.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(unsafeOkHttpClient)
-                .build();
-        CricketService cricketService = retrofit.create(CricketService.class);
-
-        Call<MatchDetailsList> call = cricketService.getMatchDetailsList();
-        call.enqueue(new Callback<MatchDetailsList>() {
-            @Override
-            public void onResponse(Call<MatchDetailsList> call, Response<MatchDetailsList> response) {
-                MatchDetailsList matchDetailsList = response.body();
-                adapter.setMatchDetailsList(matchDetailsList.matchDetailsList);
             }
+        }).attach();
+    }
 
-            @Override
-            public void onFailure(Call<MatchDetailsList> call, Throwable t) {
+
+    private class ViewPager2Adapter extends FragmentStateAdapter {
+        public ViewPager2Adapter(Fragment fragment) {
+            super(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            Fragment returnFragment = null;
+            switch (position)
+            {
+                case 0: {
+                    Fragment fragment = new LiveTabFragment();
+                    returnFragment = fragment;
+                }
+                break;
+                case 1:{
+                    Fragment fragment = new ScoreboardTabFragment();
+                    returnFragment = fragment;
+                }
+                break;
+                case 2:{
+                    Fragment fragment = new InfoTabFragment();
+                    returnFragment = fragment;
+                }
             }
-        });
+            return returnFragment;
 
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
     }
 }
